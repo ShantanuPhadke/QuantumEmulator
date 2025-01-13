b03bc0608b12ade1caeb7c11e13c98c76075cdff
Shantanu Phadke <shantanu.phadke@kellogg.northwestern.edu>
2025-01-13 12:20:54 -0800

    Added in some more complex number operations including subtraction, division, modulus and conjugate

--- complex/ComplexOperations.py
+++ complex/ComplexOperations.py
@@ -10,6 +10,26 @@ class ComplexOperations:
         return ComplexNumber(c1.real * c2.real - c1.imag * c2.imag, c1.real * c2.imag + c1.imag * c2.real)
     
     @staticmethod
-    def output_sum_and_product(self, c1: ComplexNumber, c2: ComplexNumber):
+    def __sub__(c1: ComplexNumber, c2: ComplexNumber):
+        return ComplexNumber(c1.real - c2.real, c1.imag - c2.imag)
+    
+    @staticmethod
+    def __div__(c1: ComplexNumber, c2: ComplexNumber):
+        return ComplexNumber((c1.real * c2.real + c1.imag * c2.imag) / (c2.real ** 2 + c2.imag ** 2), (c1.imag * c2.real - c1.real * c2.imag) / (c2.real ** 2 + c2.imag ** 2))
+    
+    @staticmethod
+    def __conjugate__(c: ComplexNumber):
+        return ComplexNumber(c.real, -c.imag)
+    
+    @staticmethod
+    def __modulus__(c: ComplexNumber):
+        return (c.real ** 2 + c.imag ** 2) ** 0.5
+    
+    @staticmethod
+    def ouput(c1: ComplexNumber, c2: ComplexNumber):
         print(f"Sum: {ComplexOperations.__add__(c1, c2)}")
         print(f"Product: {ComplexOperations.__mul__(c1, c2)}")
+        print(f"Difference: {ComplexOperations.__sub__(c1, c2)}")
+        print(f"Division: {ComplexOperations.__div__(c1, c2)}")
+        print(f"Conjugate (first number): {ComplexOperations.__conjugate__(c1)}")
+        print(f"Modulus (first number): {ComplexOperations.__modulus__(c1)}")
--- tests/test_complex_operations.py
+++ tests/test_complex_operations.py
@@ -12,3 +12,23 @@ def test_multiplication():
     c2 = ComplexNumber(3, 4) # 3 + 4i
     # c3 = c1 * c2 = (1 + 2i) * (3 + 4i) = 3 + 4i + 6i + 8i^2 = 3 - 8 + 10i = 10i - 5
     assert ComplexOperations.__mul__(c1, c2) == ComplexNumber(-5, 10)
+
+def test_subtraction():
+    c1 = ComplexNumber(1, 2) # 1 + 2i
+    c2 = ComplexNumber(3, 4) # 3 + 4i
+    assert ComplexOperations.__sub__(c1, c2) == ComplexNumber(-2, -2)
+
+def test_division():
+    c1 = ComplexNumber(1, 2) # 1 + 2i
+    c2 = ComplexNumber(3, 4) # 3 + 4i
+    # c3 = c1 / c2 = (1 + 2i) / (3 + 4i) = (1 + 2i)(3 - 4i) / (3 + 4i)(3 - 4i) = (3 - 4 + 6i + 8) / (9 + 16) = 11/25 + 2/25i
+    assert ComplexOperations.__div__(c1, c2) == ComplexNumber(11/25, 2/25)
+
+def test_conjugate():
+    c = ComplexNumber(1, 2) # 1 + 2i
+    assert ComplexOperations.__conjugate__(c) == ComplexNumber(1, -2)
+
+def test_modulus():
+    c = ComplexNumber(1, 2) # 1 + 2i
+    # |c| = sqrt(1^2 + 2^2) = sqrt(1 + 4) = sqrt(5)
+    assert ComplexOperations.__modulus__(c) == (5) ** 0.5
