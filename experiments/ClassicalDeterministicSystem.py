9cfa4205745751076a8ddeb378cd98613e7c4902
Shantanu Phadke <shantanu.phadke@kellogg.northwestern.edu>
2025-01-16 23:15:29 -0800

    Changed the implementation of classical deterministic systems so that the user just has to specify the transitions and we will construct the transition matrix in the constructor

--- experiments/ClassicalDeterministicSystem.py
+++ experiments/ClassicalDeterministicSystem.py
@@ -3,9 +3,18 @@ from complex.ComplexNumber import ComplexNumber
 from complex.ComplexVector import ComplexVector
 
 class ClassicalDeterministicSystem:
-    def __init__(self, start_state, transition_matrix):
+    # transitions = a list of tuples that each signify individual edges between vertices
+    # Ex: tuple (i, j) = there is an edge between vertex i and vertex j
+    # The constructor will produce the appropriate transition matrix from this.
+    def __init__(self, start_state, transitions):
         self.start_state = ComplexVector(len(start_state), [ComplexNumber(element, 0) for element in start_state])
-        self.transition_matrix = ComplexMatrix(matrix=[[ComplexNumber(element, 0) for element in row] for row in transition_matrix])
+        self.num_states = len(start_state)
+        self.transition_matrix = ComplexMatrix(matrix=[
+            [
+                ComplexNumber(1, 0) if (origin, dest) in transitions else ComplexNumber(0,0) for origin in range(self.num_states)
+            ] for dest in range(self.num_states)
+        ])
+
     
     def simulate_state(self, time_clicks):
         state = self.start_state
--- tests/test_classical_deterministic_systems.py
+++ tests/test_classical_deterministic_systems.py
@@ -4,5 +4,5 @@ from complex.ComplexVector import ComplexVector
 from complex.ComplexNumber import ComplexNumber
 
 def test_classical_deterministic_system():
-    c = ClassicalDeterministicSystem([6, 2, 1, 5, 3, 10], [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0], [0, 0, 1, 0, 0, 0], [1, 0, 0, 0, 1, 0]])
+    c = ClassicalDeterministicSystem([6, 2, 1, 5, 3, 10], [(1, 2), (5, 2), (3, 3), (2, 4), (0, 5), (4, 5)])
     assert c.simulate_state(1) == ComplexVector(6, [ComplexNumber(0, 0), ComplexNumber(0, 0), ComplexNumber(12, 0), ComplexNumber(5, 0), ComplexNumber(1, 0), ComplexNumber(9, 0)])
